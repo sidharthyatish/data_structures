b60d9901d6777bc071f6248883986cbc5a86aba8
Sidharth Yatish <Sidharth.Yathish@genesys.com>
2020-08-28 20:14:54 +0530

    Added leetcode solution to check if a LL is a palindrome

--- src/LinkedList/MainClass.java
+++ src/LinkedList/MainClass.java
@@ -18,10 +18,56 @@ public class MainClass
         tempOdd.next=evenHead;
         return oddHead;
     }
+    
+    public static Node reverseAtIndex(Node head,int index){
+        if(head==null||head.next==null) return head;
+        
+        Node temp = head;
+        while(temp!=null&&temp.next!=null&&index-->0){
+            Node next = temp.next;
+            temp.next=temp.next.next;
+            next.next = head;
+            head = next;
+        }
+        return head;
+    }
+    
+    public static boolean checkIfPalindrome(Node head){
+        if(head==null||head.next==null) return true;
+        Node fastNode = head;
+        Node midNode = head;
+        int midPosition=0;
+        while(fastNode!=null&&fastNode.next!=null){
+            fastNode=fastNode.next.next;
+            midNode=midNode.next;
+            midPosition++;
+        }
+        //if fastNode.next==null it means there were odd number of nodes. if fastNode == null it means there were odd number of nodes
+        //Move the mid node if odd number of nodes are present
+        if(fastNode!=null){
+            midNode=midNode.next;
+        }
+        
+        head = reverseAtIndex(head,midPosition-1); //have to reverse just before the mid (reverse)mid(rest-half)
+        Node temp = head;
+        
+        while(midNode!=null){
+            
+            if(temp.data!=midNode.data) return false;
+            temp=temp.next;
+            midNode=midNode.next;
+        }
+        return true;
+    }
     public static void main(String[] args)
     {
-        testBasicOperations();
-        testOddEvenLL();
+//        testBasicOperations();
+//        testOddEvenLL();
+        LinkedList linkedList = new LinkedList();
+        linkedList.constructLinkedList(new int[]{1,2,1});
+        linkedList.printList();
+        System.out.println(checkIfPalindrome(linkedList.head));
+        
     }
     
     private static void testBasicOperations()
