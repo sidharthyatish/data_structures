eb2155986f1519e5a75f6f469a726f234e4b0cf7
Sidharth Yatish <Sidharth.Yathish@genesys.com>
2020-09-03 14:03:25 +0530

    Binary tree inorder traversal iterative method - my solution added
    This is my solution. Solution from geeks looks different. This however is accepted in leetcode

--- src/Tree/BinaryTree.java
+++ src/Tree/BinaryTree.java
@@ -2,6 +2,7 @@ package Tree;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 class Node{
     int data;
@@ -45,6 +46,45 @@ public class BinaryTree
             if(poppedNode.left != null) levelQueue.add(poppedNode.left);
             if(poppedNode.right != null) levelQueue.add(poppedNode.right);
         }
+        
+    }
+    
+    void printInOrderIterative(Node root){
+        //Inorder is Left,Root,Right
+        //Since right is printed last, it must be put first into stack
+        
+        Stack<Node> inOrderStack = new Stack<>();
+        if(root == null) return;
+        Node leftNode = root.left;
+        Node rightNode = root.right;
+        root.left=null;
+        root.right=null;
+        if(rightNode!=null)
+            inOrderStack.push(rightNode);
+        
+        inOrderStack.push(root);
+        
+        if(leftNode!=null)
+            inOrderStack.push(leftNode);
+        
+        while(!inOrderStack.empty()){
+            Node poppedNode = inOrderStack.pop();
+            leftNode = poppedNode.left;
+            rightNode = poppedNode.right;
+            if (leftNode == null && rightNode == null)
+            {
+                System.out.print(poppedNode.data+" ");
+                poppedNode=null;
+            }
+            else{
+                poppedNode.left=null;
+                poppedNode.right=null;
+            }
+            if(rightNode!=null) inOrderStack.push(rightNode);
+            if(poppedNode!=null) inOrderStack.push(poppedNode);
+            if(leftNode!=null) inOrderStack.push(leftNode);
+        }
+        System.out.println();
         
     }
 }
